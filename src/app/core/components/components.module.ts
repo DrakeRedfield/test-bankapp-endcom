9372523a8c50e3f3a85785d6ecadb588e3dad08f
Angel Antonio Barco Alfaro <drake.redfield@hotmail.com>
2022-07-17 15:50:57 -0500

    declare components into a module

---
+++ src/app/core/components/components.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from "@angular/core";
+import { BaseLayoutComponent } from './base-layout/base-layout.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { FundsCardComponent } from './cards/funds-card/funds-card.component';
+import { CardBankComponent } from './cards/card-bank/card-bank.component';
+import { LogoComponent } from './logo/logo.component';
+import { UserDataComponent } from './user-data/user-data.component';
+import { MovementsTableComponent } from './movements-table/movements-table.component';
+import { CommonModule } from '@angular/common';
+
+@NgModule({
+    declarations: [
+    BaseLayoutComponent,
+    HeaderComponent,
+    FooterComponent,
+    FundsCardComponent,
+    CardBankComponent,
+    LogoComponent,
+    UserDataComponent,
+    MovementsTableComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class ComponentsModule {}
