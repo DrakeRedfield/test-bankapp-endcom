df51def93492a14f108e5ed1f1139ba78e28e4ab
Angel Antonio Barco Alfaro <drake.redfield@hotmail.com>
2022-07-17 17:52:58 -0500

    header component layouted

--- src/app/core/components/header/header.component.ts
+++ src/app/core/components/header/header.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { IMenuOption } from '../../../utils/interfaces/menu.interfaces';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent {
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  menuOptions: IMenuOption[] = [
+    {
+      text:'MIS CUENTAS',
+      isSelected: true
+    },
+    {
+      text:'ENVIAR DINERO',
+      isSelected: false
+    }
+  ]
 }
---
+++ src/app/utils/interfaces/menu.interfaces.ts
@@ -0,0 +1,4 @@
+export interface IMenuOption {
+    text: string;
+    isSelected: boolean;
+}
