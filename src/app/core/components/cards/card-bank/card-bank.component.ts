9aa83b2a4a551a997a8fc2bf44207fd9ac5a85bd
Angel Antonio Barco Alfaro <drake.redfield@hotmail.com>
2022-07-17 15:40:05 -0500

    cards components generated

---
+++ src/app/core/components/cards/card-bank/card-bank.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-card-bank',
+  templateUrl: './card-bank.component.html',
+  styleUrls: ['./card-bank.component.scss']
+})
+export class CardBankComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/core/components/cards/funds-card/funds-card.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-funds-card',
+  templateUrl: './funds-card.component.html',
+  styleUrls: ['./funds-card.component.scss']
+})
+export class FundsCardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
