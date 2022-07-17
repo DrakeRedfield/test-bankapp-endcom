12500b83fb761c4c8e1b1e14aa5fcca84b8daf4e
Angel Antonio Barco Alfaro <drake.redfield@hotmail.com>
2022-07-17 18:41:02 -0500

    funds section component created

---
+++ src/app/core/components/funds-section/funds-section.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-funds-section',
+  templateUrl: './funds-section.component.html',
+  styleUrls: ['./funds-section.component.scss']
+})
+export class FundsSectionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
