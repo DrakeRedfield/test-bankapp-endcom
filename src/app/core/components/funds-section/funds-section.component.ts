431506f04a36594312c45778f5933decd89da2cc
Angel Antonio Barco Alfaro <drake.redfield@hotmail.com>
2022-07-18 14:23:03 -0500

    integrate account service

--- src/app/core/components/funds-section/funds-section.component.ts
+++ src/app/core/components/funds-section/funds-section.component.ts
@@ -1,26 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AccountService } from '../../services/account.service';
+import { IFundData } from '../../../utils/interfaces/requests-http/requests-http.interfaces';
 
 @Component({
   selector: 'app-funds-section',
   templateUrl: './funds-section.component.html',
   styleUrls: ['./funds-section.component.scss']
 })
-export class FundsSectionComponent {
+export class FundsSectionComponent implements OnInit {
 
   cardDatalist = [
     {
-      mount: 1234,
+      mount: 0,
       text: 'SALDO GENERAL EN CUENTAS'
     },
     {
-      mount: 1234,
+      mount: 0,
       text: 'TOTAL DE INGRESOS',
       isTextGray: true
     },
     {
-      mount: 1234,
+      mount: 0,
       text: 'TOTAL DE GASTOS',
       isTextGray: true
     }
   ];
+
+  constructor(
+    private accountService: AccountService,
+  ) {}
+
+  ngOnInit(): void {
+    this.getData();
+  }
+
+  getData() {
+    this.accountService.getFounds().subscribe( ( ( fundsData: IFundData ) => {
+      this.cardDatalist[0].mount = fundsData.saldoGeneral;
+      this.cardDatalist[1].mount = fundsData.ingresos;
+      this.cardDatalist[2].mount = fundsData.gastos;
+    }));
+  }
 }
